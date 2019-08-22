b9f87fb2f3db17421de00cf893bdcc8800322568
Sunil Singh <sunilp896@gmail.com>
2019-08-22 07:01:25 +0530

    added arguments to domain and updated gitignore

--- pass.cpp
+++ pass.cpp
@@ -1,16 +1,38 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/InstIterator.h"
+#include <vector>
 
 using namespace llvm;
-namespace{
-    struct LA : public FunctionPass{
-	static char ID;
-	LA(): FunctionPass(ID){}
+using namespace std;
 
-	virtual bool runOnFunction(Function &F){
-	    errs() << "Hello Function Pass\n";
-	    return false;
-	}
+namespace{
+    class LA : public FunctionPass{
+	public:	
+	    static char ID;
+		vector<Value*> domain;
+	    LA(): FunctionPass(ID){}
+
+	    virtual bool runOnFunction(Function &F){
+		
+		errs() << F.getName() << '\n';
+
+		for (auto arg_it = F.arg_begin(); arg_it != F.arg_end(); ++arg_it){
+			domain.push_back(arg_it);
+		}
+		
+
+		for (inst_iterator iit = inst_begin(F); iit!=inst_end(F); ++iit){
+			Value* v = &*iit;
+			errs() << *v << '\n';
+			// errs() << *iit << '\n';
+			
+		}
+		
+
+		return false;
+	    }
     };
 }
 

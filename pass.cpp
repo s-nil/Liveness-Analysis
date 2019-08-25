40227ed6b5e518c03a1928da94180ee6e95cba21
Sunil Singh <sunilp896@gmail.com>
2019-08-25 07:12:48 +0530

    added definitions in domain

--- pass.cpp
+++ pass.cpp
@@ -1,35 +1,82 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Pass.h"
+#include "llvm/ADT/BitVector.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/InstIterator.h"
+#include "llvm/IR/Instructions.h"
 #include <vector>
 
 using namespace llvm;
 using namespace std;
 
 namespace{
+	void printBitvector(BitVector b){
+		for(int i = 0; i < b.size(); ++i){
+			if(b[i])
+				errs() << "1";
+			else
+				errs() << "0";
+		}
+		errs() << '\n';
+	}
+
+	// class {};
+
+	class FlowResultofBB{
+		public:
+			BitVector in;
+			BitVector out;
+
+			FlowResultofBB(){}
+			FlowResultofBB(BitVector i, BitVector o){
+				in = i;
+				out = o;
+			}
+	};
+
+	class FlowResult{
+		public:
+			DenseMap<BasicBlock*, FlowResultofBB> fr;
+			FlowResult(){}
+	};
+
     class LA : public FunctionPass{
 	public:	
 	    static char ID;
 		vector<Value*> domain;
-	    LA(): FunctionPass(ID){}
+	    BitVector boundaryCondition;
+		BitVector InitBlockCond;
+		LA(): FunctionPass(ID){}
 
 	    virtual bool runOnFunction(Function &F){
-		
+		domain.clear();
 		errs() << F.getName() << '\n';
 
 		for (auto arg_it = F.arg_begin(); arg_it != F.arg_end(); ++arg_it){
 			domain.push_back(arg_it);
 		}
 		
-
 		for (inst_iterator iit = inst_begin(F); iit!=inst_end(F); ++iit){
 			Value* v = &*iit;
-			errs() << *v << '\n';
-			// errs() << *iit << '\n';
-			
+
+			if(isa<StoreInst>(&*iit)){
+				Value *vv = (((StoreInst*)v)->getPointerOperand());
+				if(find(domain.begin(),domain.end(),vv) == domain.end()){
+					domain.push_back(vv);
+				}
+			}
+			else if(isa<Instruction>(&*iit)){
+				string str;
+				raw_string_ostream rso(str);
+				rso << *iit;
+
+				if(str.size()>2 && str[2]=='%'){
+					if(find(domain.begin(),domain.end(),v) == domain.end()){
+						domain.push_back(v);
+					}
+				}
+			}
 		}
-		
 
 		return false;
 	    }
